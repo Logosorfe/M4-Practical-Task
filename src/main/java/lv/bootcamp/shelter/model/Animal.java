cd3a93399e6410fb4ab0c76bbfc0173588bef47e
elina.rostoka <elina.rostoka@accenture.com>
2026-06-28 18:27:50 +0300

    Lombok is added to animal class

--- src/main/java/lv/bootcamp/shelter/model/Animal.java
+++ src/main/java/lv/bootcamp/shelter/model/Animal.java
@@ -1,39 +1,20 @@
 package lv.bootcamp.shelter.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Animal {
+
     private final String name;
     private final String species;
     private final Integer age;
     private final boolean vaccinated;
     private final LocalDate intakeDate;
 
-    public Animal(String name, String species, Integer age, boolean vaccinated, LocalDate intakeDate) {
-        this.name = name;
-        this.species = species;
-        this.age = age;
-        this.vaccinated = vaccinated;
-        this.intakeDate = intakeDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSpecies() {
-        return species;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public boolean isVaccinated() {
-        return vaccinated;
-    }
-
-    public LocalDate getIntakeDate() {
-        return intakeDate;
-    }
 }
